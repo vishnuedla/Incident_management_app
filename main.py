88f7dab04e22ad020a3b44a0d34349ddeee0dbb5
Vishnu <edlavishnu@aol.com>
2026-08-17 16:35:32 +0530

    resolved confilcts

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pika
 import sys
 import os
@@ -65,71 +64,3 @@ def read_incident():
 
 
 
-=======
-import pika
-import sys
-import os
-from  rabbitmq_code import send_message_to_queue
-import psycopg2
-import logging
-from fastapi import FastAPI
-from database import connection_database 
-
-
-app = FastAPI()
-
-app_logger = logging.getLogger("application_function_logger")
-app_logger.setLevel(logging.INFO)
-
-# File handler for this function
-fh = logging.FileHandler("application.log")
-fh.setLevel(logging.INFO)
-
-# Formatter
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-
-# Attach handler to the logger
-app_logger.addHandler(fh)
-
-@app.post("/incident_create")
-def create_incident(DEPARTMENT,ISSUE,STATUS):
-    try:
-      connection = connection_database()
-    except psycopg2.OperationalError as e:
-      app_logger.critical(f"Database connection error: {e}")
-    else:
-      app_logger.info("Database connection established for incident creation")
-      cur=connection.cursor()
-      cur.execute("INSERT INTO incident_create (incident_id, DEPARTMENT, ISSUE, STATUS) VALUES ('inc' || nextval('incident_seq') , %s, %s, %s)", (DEPARTMENT, ISSUE, STATUS))
-      connection.commit()
-      # Fetch the incident_id of the newly created incident
-      cur.execute("SELECT currval('incident_seq')")
-      INCIDENT_ID = cur.fetchone()[0]
-      INCIDENT= f"inc{INCIDENT_ID}"
-      app_logger.info(f"Incident {INCIDENT} created successfully")
-      cur.close()
-      connection.close()
-      app_logger.info("Database connection closed after incident creation")
-      # Send message to RabbitMQ queue
-      send_message_to_queue(INCIDENT,DEPARTMENT, ISSUE, STATUS)
-
-@app.get("/incident_read")
-def read_incident():
-    try:
-        connection = connection_database()
-    except psycopg2.OperationalError as e:
-        app_logger.critical(f"Database connection error: {e}")
-    else:
-      app_logger.info("Database connection established for incident reading")
-      cur=connection.cursor()
-      cur.execute("SELECT * FROM incident_create")
-      result = cur.fetchall()
-      cur.close()
-      connection.close()
-      app_logger.info("Database connection closed after incident reading")
-      return result
-
-
-
->>>>>>> 9a665371f4ef17493b946497e91706e32aace616
